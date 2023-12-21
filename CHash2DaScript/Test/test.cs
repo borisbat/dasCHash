13ce2ed1204ed7c459feafee2a92dae538e1b06b
Dima Granetchi <system.grand@gmail.com>
2023-12-21 18:26:04 +0200

    interfaces

--- CHash2DaScript/Test/test.cs
+++ CHash2DaScript/Test/test.cs
@@ -11,7 +11,12 @@ namespace HelloWorld
         public int x;
     };
 
-    class Foo
+    interface IFoo
+    {
+        void bar();
+    }
+
+    class Foo : IFoo
     {
         static int count = 13;
 
